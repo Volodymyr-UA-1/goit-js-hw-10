b1d28a7517d06178da3b7b61560325c53870bb90
Volodymyr <salashchenkovladymyr@gmail.com>
2025-11-15 14:34:21 +0200

    add libraries

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,2 +1,39 @@
+// Описаний в документації
+import flatpickr from "flatpickr";
+// Додатковий імпорт стилів
+import "flatpickr/dist/flatpickr.min.css";
+
+// . Тому оголоси поза межами методу let змінну, 
+// наприклад, userSelectedDate, 
+// і після валідації її в методі onClose() 
+// на минуле/майбутнє запиши обрану дату в цю let змінну.
 
 
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
